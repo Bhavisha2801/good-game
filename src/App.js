ba27a7f4fbcccd3aed9e8f28912e82bba61bb650
Bhavisha2801 <nayibhavisha@gmail.com>
2022-06-16 21:43:00 +0530

    "inserted good game assignment"

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import data from './data.json'
 
 function App() {
+
+  const [info , setInfo] = useState(data)
+
+  const one = info.level1
+  const two = info.level2
+  const three = info.level3
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='level1-main'>
+      {
+        one.map(item => (
+          <div className='container-one' key={item.id}>
+            <div className='level'>{item.player}</div>
+            <div><hr className='horizontal' /></div>
+          </div>
+          
+        ))
+      }
+      </div>
+      <div className='level2-main'>
+      <div><hr className='vertical' /></div>
+      {
+        two.map(item => (
+          <div className='container-one' key={item.id}>
+            <div className='level'>{item.player}</div>
+            <div><hr className='horizontal' /></div>
+          </div>
+        ))
+      }
+      <div><hr className='vertical' /></div>
+      </div>
+      <div className='level3-main'>
+      {
+        three.map(item => (
+          <div className='level' key={item.id}>{item.player}</div>
+        ))
+      }
+      </div>
     </div>
   );
 }
